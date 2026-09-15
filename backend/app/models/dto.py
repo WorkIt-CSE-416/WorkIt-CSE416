b13591ef8a759a7b3476da0fb24b6061ff24c504
xiangliu1103 <xiang.liu.1@stonybrook.edu>
2026-09-15 17:53:55 -0400

    KAN 22: complete more profile tables

--- backend/app/models/dto.py
+++ backend/app/models/dto.py
@@ -1,4 +1,31 @@
 '''
 file that holds data objects, such as key words, enums, tec.
 '''
-from pydantic import BaseModel as PydanticBase
+import enum 
+
+
+# size range for company 
+class company_size_range(str,enum.Enum):
+    ONE_TO_FIFTY: "1_50"
+    FIFTY_TO_TWO_HUNDRED: "51_200"
+    TWO_HUNDRED_TO_FIVE_HUNDRED: "201_500"
+    TOUSAND: "501_1000"
+    FIVE_THOUSAND: "1001_5000"
+    TEN_THOUSAND: "5001_10000"
+    LARGE_THOUSAND: "10000_"
+
+class company_role(str, enum.Enum): 
+    '''
+    different possible roles for company memebership table 
+    '''
+    COMPANY_OWNER= "Owner"
+    COMPANY_ADMIN= "Admin"
+    COMPANY_RECRUITER= "Recruiter"
+
+class profile_status(str, enum.Enum):
+    '''
+    different possible status for a profile to be in
+    '''
+    ACTIVE= "Active"
+    INVITED= "Invited"
+    DISABLED = "Disabled"
--- backend/app/models/profiles.py
+++ backend/app/models/profiles.py
@@ -6,10 +6,11 @@ import datetime
 import uuid 
 from sqlalchemy import ForeignKey
 from sqlalchemy import String, DateTime, func
-from sqlalchemy.orm import DeclarativeBase, Mapped,mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped,mapped_column 
+from models.dto import company_size_range, company_role, profile_status
 
 class Base(DeclarativeBase): 
-    # standard base 
+    # standard base by default 
     pass 
 
 class BaseModel(Base): 
@@ -30,21 +31,47 @@ class Profile(BaseModel):
     '''
     __tablename__ = "profiles"
     id:Mapped[uuid.UUID] = mapped_column(primary_key=True)
-    email: Mapped[str]
-    full_name: Mapped[str] 
-    phone_number: Mapped[Optional[str]]
+    email: Mapped[str] = mapped_column(String(100))
+    full_name: Mapped[str] =mapped_column(String(50))
+    phone_number: Mapped[Optional[str]] = mapped_column(String(30))
     avatar_url: Mapped[Optional[str]]
 
-class Applicant_Profile(BaseModel): 
+class Applicant_Profile(Profile): 
     '''
     schema for job applicants 
     '''
     __tablename__= "applicant_profiles"
-    profile_id: Mapped[uuid.UUID]= mapped_column(
-                ForeignKey("profiles.id", ondelete="CASCADE")
-    )
-    headline: Mapped[Optional[str]] 
-    linkedin_url: Mapped[Optional[str]]
+    headline: Mapped[Optional[str]] = mapped_column(String(50))
+    linkedin_url: Mapped[Optional[str]] 
     portfolio_url: Mapped[Optional[str]]
     github_url: Mapped[Optional[str]]
     other_url: Mapped[Optional[str]]    # add another url for extra options 
+
+
+class Company_Profile(BaseModel): 
+    '''
+    schema for company profile (the company information)
+    separate from individual recruiter under the company
+    '''
+    __tablename__="company_profiles"
+    id:Mapped[uuid.UUID] = mapped_column(primary_key=True)
+    company_name: Mapped[str] = mapped_column(String(255))
+    slug:Mapped[Optional[str]]
+    website_url: Mapped[Optional[str]]
+    contact_email:Mapped[str] 
+    phone_number: Mapped[Optional[str]] = mapped_column(String(30)) 
+    logo_url: Mapped[Optional[str]]
+    description: Mapped[Optional[str]]= mapped_column(String(250))
+    size_range: Mapped[company_size_range]
+
+
+class Company_Membership(Profile):
+    '''
+    individual profile under company (i.e. recruiter, etc.)
+    '''
+    company_id: Mapped[uuid.UUID]= mapped_column(
+        ForeignKey("company_profiles.id", ondelete="CASCADE")
+    )
+    role:Mapped[company_role]
+    status: Mapped[profile_status]
+    headline: Mapped[str] = mapped_column(String(50))
